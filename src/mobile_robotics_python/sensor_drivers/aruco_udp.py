30a51be0135912a56fa285f21e07b966256d62b3
flaviogheri <gheriarnaudflavio@gmail.com>
2022-08-11 11:46:25 +0100

    added marker_id in config + save aruco info to RobotStateMessage

--- src/mobile_robotics_python/sensor_drivers/aruco_udp.py
+++ src/mobile_robotics_python/sensor_drivers/aruco_udp.py
@@ -3,7 +3,7 @@ from threading import Thread
 
 from mobile_robotics_python import Rate
 from mobile_robotics_python.messages import RobotStateMessage
-
+import json
 
 class ArUcoUDP:
     def __init__(self, params):
@@ -19,14 +19,19 @@ class ArUcoUDP:
         self.client.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
         self.client.settimeout(params["timeout"])
         self.client.bind(("", params["port"]))
+        self.aruco_id = params['aruco_id']
         self.th = Thread(target=self.loop, daemon=True)
         self.th.start()
+
+        # -- data recieved
+        self.data = list(range(8))
 
     def loop(self):
         r = Rate(10)
         while True:
-            try:
-                self.data, _ = self.client.recvfrom(1024)
+            broadcast_data, _ = self.client.recvfrom(1024)
+            result = json.loads(broadcast_data)
+            self.data = result[f'{self.aruco_id}']
             except Exception as e:
                 print("Got exception trying to recv %s" % e)
                 raise StopIteration
@@ -34,6 +39,16 @@ class ArUcoUDP:
 
     def read(self) -> RobotStateMessage:
         # TODO convert self.data to RobotStateMessage
+        
+        msg = RobotStateMessage()
+        msg.stamp_s = self.data(0)
+        msg.x_m = self.data(2)
+        msg.y_m = self.data(3)
+        msg.z_m = self.data(4)
+        msg.roll_rad = self.data(5)
+        msg.pitch_rad = self.data(6)
+        msg.yaw_rad = self.data(7)
+
         pass
 
     def __del__(self):
